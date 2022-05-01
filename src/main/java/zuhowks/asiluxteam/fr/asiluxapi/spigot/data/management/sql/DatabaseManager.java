0dbe9ae14123960111466aa02061cb21bf8edb12
ZUHOWKS <jojo.vilard38@gmail.com>
2022-05-01 20:21:44 +0200

    Implement SQL Manager (can't use them for the moment in API).

---
+++ src/main/java/zuhowks/asiluxteam/fr/asiluxapi/spigot/data/management/sql/DatabaseManager.java
@@ -0,0 +1,62 @@
+package zuhowks.asiluxteam.fr.asiluxapi.spigot.data.management.sql;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import zuhowks.asiluxteam.fr.asiluxapi.spigot.AsiluxAPI;
+
+import java.io.File;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public enum DatabaseManager {
+    //Use main database
+    PLAYERS_ACCOUNT("players-account");
+
+    private SQLDatabase SQLDatabase;
+
+    DatabaseManager(String database) {
+        FileConfiguration configuration = AsiluxAPI.INSTANCE.getConfig();
+        this.SQLDatabase = new SQLDatabase(
+                new DatabaseCredantial(
+                        configuration.getString("sql-manager." + database + ".host"),
+                        configuration.getInt("sql-manager." + database + ".port"),
+                        configuration.getString("sql-manager." + database + ".database"),
+                        configuration.getString("sql-manager." + database + ".username"),
+                        configuration.getString("sql-manager." + database + ".password")
+        ));
+    }
+
+    public SQLDatabase getDatabaseAccess() {
+        return SQLDatabase;
+    }
+
+    public static void initAllDatabaseConnection () {
+        for (DatabaseManager databaseManager : values()) {
+            databaseManager.SQLDatabase.initPool();
+            if (databaseManager.equals(DatabaseManager.PLAYERS_ACCOUNT) && !AsiluxAPI.INSTANCE.getServer().spigot().getSpigotConfig().getBoolean("settings.bungeecord")) {
+                try {
+                    PreparedStatement ps = databaseManager.getDatabaseAccess().getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS `players_account` (\n" +
+                            "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+                            "  `uuid` varchar(36) CHARACTER SET latin1 NOT NULL,\n" +
+                            "  `rank` varchar(32) CHARACTER SET latin1 NOT NULL,\n" +
+                            "  `coins` int(11) NOT NULL,\n" +
+                            "  `level` smallint(6) NOT NULL,\n" +
+                            "  `xp` int(11) NOT NULL,\n" +
+                            "  `mmr` int(11),\n" +
+                            "  PRIMARY KEY (`id`)," +
+                            "  UNIQUE (`uuid`)" +
+                            ") ENGINE=InnoDB DEFAULT CHARSET=utf8"
+                    );
+                    ps.execute();
+                } catch (SQLException throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void closeAllDatabaseConnection () {
+        for (DatabaseManager databaseManager : values()) {
+            databaseManager.SQLDatabase.closePool();
+        }
+    }
+}
---
+++ src/main/java/zuhowks/asiluxteam/fr/asiluxapi/spigot/data/management/sql/SQLDatabase.java
@@ -0,0 +1,48 @@
+package zuhowks.asiluxteam.fr.asiluxapi.spigot.data.management.sql;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SQLDatabase {
+    private DatabaseCredantial credantial;
+    private HikariDataSource hikariDataSource;
+
+    public SQLDatabase(DatabaseCredantial databaseManager) {
+        this.credantial = databaseManager;
+    }
+
+    public void initPool() {
+        final HikariConfig hikariConfig = new HikariConfig();
+
+        hikariConfig.setMaximumPoolSize(20);
+        hikariConfig.setJdbcUrl(credantial.toURL());
+        hikariConfig.setUsername(credantial.getUsername());
+        hikariConfig.setPassword(credantial.getPassword());
+        hikariConfig.setMaxLifetime(600000L);
+        hikariConfig.setIdleTimeout(300000L);
+        hikariConfig.setLeakDetectionThreshold(300000L);
+        hikariConfig.setConnectionTimeout(10000L);
+
+        this.hikariDataSource = new HikariDataSource(hikariConfig);
+    }
+
+    public void closePool() {
+        this.hikariDataSource.close();
+    }
+
+    public Connection getConnection () {
+        if (this.hikariDataSource == null) {
+            System.out.println("Not connected");
+            this.initPool();
+        }
+        try {
+            return this.hikariDataSource.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+}
