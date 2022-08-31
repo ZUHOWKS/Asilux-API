10b19f475177459e13a2c1f1e708c74455dbb0d6
Joris VILARDELL <jojo.vilard38@gmail.com>
2022-08-31 19:09:51 +0200

    Fix bug de creation de la base de donnée MySQL.

--- src/main/java/zuhowks/asiluxteam/fr/asiluxapi/spigot/core/AccountProvider.java
+++ src/main/java/zuhowks/asiluxteam/fr/asiluxapi/spigot/core/AccountProvider.java
@@ -69,7 +69,7 @@ public class AccountProvider {
         Bukkit.getScheduler().runTaskAsynchronously(AsiluxAPI.INSTANCE, () -> {
             try {
                 final PreparedStatement ps = DatabaseManager.PLAYERS_ACCOUNT.getDatabaseAccess().getConnection().prepareStatement(
-                        "UPDATE `players_account` SET rank=?, coins=?, level=?, xp=?, mmr=?, lang=? WHERE id=? AND uuid=?"
+                        "UPDATE `players_account` SET ranked=?, coins=?, level=?, xp=?, mmr=?, lang=? WHERE id=? AND uuid=?"
                 );
                 ps.setString(1,account.getRank().toLowerCase(Locale.ROOT));
                 ps.setInt(2, account.getCoins());
@@ -121,7 +121,7 @@ public class AccountProvider {
                 if (rs.next()) {
 
                     final int id = rs.getInt("id");
-                    final String rank = rs.getString("rank");
+                    final String rank = rs.getString("ranked");
                     final int coins = rs.getInt("coins");
                     final int level = rs.getInt("level");
                     final int xp = rs.getInt("xp");
@@ -146,7 +146,7 @@ public class AccountProvider {
 
     public Account registerAccount(UUID uuid, Connection connection) throws SQLException {
         final Account account = DEFAULT_ACCOUNT.clone();
-        final PreparedStatement ps = connection.prepareStatement("INSERT INTO players_account (uuid, rank, coins, level, xp, mmr, lang) VALUES (?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        final PreparedStatement ps = connection.prepareStatement("INSERT INTO players_account (uuid, ranked, coins, level, xp, mmr, lang) VALUES (?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         ps.setString(1, uuid.toString());
         ps.setString(2, account.getRank());
         ps.setInt(3, account.getCoins());
